476de7d1117d943058b5da0a3b1b31e529075bed
Malcolm Kumwenda <malcolm@mdevsa.com>
2017-08-04 09:51:39 +0200

    Initial Commit

---
+++ Thread Tweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thread Tweets
+//
+//  Created by Malcolm Kumwenda on 2017/08/04.
+//  Copyright © 2017 mDevSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
